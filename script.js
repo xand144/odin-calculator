25bdeb60be10339e48ae7c52573cc3fc65f19d53
xand144 <174009470+xand144@users.noreply.github.com>
2026-05-01 17:45:24 +0100

    Add operate function & basic calculator GUI

--- script.js
+++ script.js
@@ -14,6 +14,10 @@ function divide(a, b) {
     return a / b;
 }
 
-let operand1;
-let operator;
-let operand2;
+function operate(operator, n1, n2) {
+    return operator(n1, n2);
+}
+
+let operator = null;
+let n1 = 0;
+let n2 = 0;
