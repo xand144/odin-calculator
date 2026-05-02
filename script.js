8628bd620c251de07d47aa7d3d6a9a7439b01c9e
xand144 <174009470+xand144@users.noreply.github.com>
2026-05-02 12:55:11 +0100

    Add more variables & operator event handler
    * Create a variable for the calculator display & each type of button.
    * Add click event handler for operator buttons, which checks id of the
      button clicked, & sets and displays corresponding operator.
    * Add operatorDeclared variable to track whether further numeric inputs
      should be added to the first or second operand.

--- script.js
+++ script.js
@@ -14,10 +14,47 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(operator, n1, n2) {
-    return operator(n1, n2);
+function operate(operatorSet, n1, n2) {
+    return operator(+n1, +n2);
 }
 
-let operator = null;
-let n1 = 0;
-let n2 = 0;
+let operatorSet = null;
+let operatorSymbol = "";
+let n1 = "";
+let n2 = "";
+let operatorDeclared = false;
+
+const operators = document.querySelectorAll(".operator");
+const numbers = document.querySelectorAll(".number");
+const clear = document.querySelector("#clear");
+const equals = document.querySelector("#equals");
+const display = document.querySelector("#display");
+const operation = () => `${n1} ${operatorSymbol} ${n2}`;
+const updateDisplay = () => display.textContent = operation;
+
+operators.forEach(operator => {
+    operator.addEventListener("click", e => {
+        if (n1 !== "" && operatorDeclared === false) {
+            switch (e.target.id) {
+                case "addition":
+                    operatorSet = add;
+                    operatorSymbol = "+"
+                    break;
+                case "subtraction":
+                    operatorSet = subtract;
+                    operatorSymbol = "-";
+                    break;
+                case "multiplication":
+                    operatorSet = multiply;
+                    operatorSymbol = "×";
+                    break;
+                case "division":
+                    operatorSet = divide;
+                    operatorSymbol = "÷"
+                    break;
+            }
+            operatorDeclared = true;
+            display.textContent = operation();
+        }
+    })
+})
